ae2e5cec977e4ef21ba5b20ebbc05c2465542de4
Daniel <daniel.ararat@ias.com.co>
2023-04-11 18:19:59 -0500

    aplicacion primer commit, parte grafica completa event listener de los primeros botones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import logod from './images/logo-d.png';
+import Boton from './components/Boton';
+import Pantalla from './components/Pantalla';
+import BotonClear from './components/BotonClear';
+import { useState } from 'react';
 
 function App() {
+
+  const [entrada, setEntrada] = useState('');
+
+  const agregarEntrada = val => {
+    setEntrada(entrada + val);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='dart-logo-contenedor'>
+        <img
+          src={logod}
+          className='dart-logo'
+          alt='Logo dart' />        
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={entrada} />
+        <div className='fila'>
+          <Boton manejarClic={agregarEntrada}>1</Boton>
+          <Boton manejarClic={agregarEntrada}>2</Boton>
+          <Boton manejarClic={agregarEntrada}>3</Boton>
+          <Boton manejarClic={agregarEntrada}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarEntrada}>4</Boton>
+          <Boton manejarClic={agregarEntrada}>5</Boton>
+          <Boton manejarClic={agregarEntrada}>6</Boton>
+          <Boton manejarClic={agregarEntrada}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarEntrada}>7</Boton>
+          <Boton manejarClic={agregarEntrada}>8</Boton>
+          <Boton manejarClic={agregarEntrada}>9</Boton>
+          <Boton manejarClic={agregarEntrada}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarEntrada}>=</Boton>
+          <Boton manejarClic={agregarEntrada}>0</Boton>
+          <Boton manejarClic={agregarEntrada}>.</Boton>
+          <Boton manejarClic={agregarEntrada}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
+      </div>        
     </div>
   );
 }
